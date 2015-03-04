174d4e04439de7a18e52d4629adf216681f0a9fe
Andrea Venuta <venutawebdesign@gmail.com>
2015-03-04 16:15:52 +0100

    .

--- _source/09-polygon-insetting/script.js
+++ _source/09-polygon-insetting/script.js
@@ -17,10 +17,10 @@
   canvas.height = h;
 
   var roads = (function() {
-    var g = [], edges = [], S = 5, ident = 0;
+    var g = [], edges = [], S = 5, ident = 0, q = 1;
 
     for(var y = 0; y < S; y++) for(var x = 0; x < S; x++) {
-      var p = g[y * S + x] = { x: x + Math.random() * .4, y: y + Math.random() * .4, conns: [] };
+      var p = g[y * S + x] = { x: x + q * Math.random() * .4, y: y + q * Math.random() * .4, conns: [] };
       if(x > 0) {
         p.conns.push(g[y * S + x - 1]);
         g[y * S + x - 1].conns.push(p);
@@ -151,7 +151,6 @@
     }
     out.push(insetCorner(b, c, poly[ 0 ], dist));
 
-    ctx.fillStyle = 'white';
     ctx.beginPath();
     ctx.moveTo(out[0].x * 128 + 128, out[0].y * 128 + 128);
     for(var i = 1; i < out.length; i++) {
@@ -195,9 +194,13 @@
   }
 
   // http://alienryderflex.com/intersect/
-  var lineIntersection = function(a1, a2, b1, b2) {
+  var lineIntersection = function(A1, A2, B1, B2) {
 
-    var dist, cos_, sin_, nx, p;
+    var dist, cos_, sin_, nx, p,
+        a1 = { x: A1.x, y: A1.y },
+        a2 = { x: A2.x, y: A2.y },
+        b1 = { x: B1.x, y: B1.y },
+        b2 = { x: B2.x, y: B2.y };
 
     // Translate by -a1
     a2.x -= a1.x; b1.x -= a1.x; b2.x -= a1.x;
@@ -244,7 +247,91 @@
     ctx.fill();
   }
 
-  for(var i = 0; i < polys.length; i++)
-    insetPolygon(polys[i], 0.1);
+  var lerp = function(a, b, t) { return (1 - t) * a + t * b; }
+
+  var line = function(a,b) {
+    ctx.moveTo(a.x * 128 + 128, a.y * 128 + 128);
+    ctx.lineTo(b.x * 128 + 128, b.y * 128 + 128);
+  }
+  var rect = function(a, b, c, d) {
+    ctx.moveTo(a.x * 128 + 128, a.y * 128 + 128);
+    ctx.lineTo(b.x * 128 + 128, b.y * 128 + 128);
+    ctx.lineTo(c.x * 128 + 128, c.y * 128 + 128);
+    ctx.lineTo(d.x * 128 + 128, d.y * 128 + 128);
+  }
+
+  // Inspired to https://www.cs.purdue.edu/cgvlab/papers/aliaga/eg2012.pdf
+  var subdivideStrip = function(block, strip) {
+    var points = [], quads = [], i1, i2, i3, 
+        b1, b2, dx, dy, i, j, m, n, p, len;
+
+    ctx.beginPath();
+    for(i = 0, n = block.length; i < n; i++) {
+      i1 = (i + 1) % n;
+      i2 = (i + 2) % n;
+      i3 = (i - 1 + n) % n;
+      b1 = lineIntersection(block[i], block[i1],
+                            strip[i3], strip[i]);
+      b2 = lineIntersection(block[i], block[i1],
+                            strip[i1], strip[i2]);
+      dx = b1.x - b2.x;
+      dy = b1.y - b2.y;
+      len = Math.sqrt(dx * dx + dy * dy);
+      m = ~~(Math.random() * 3 + 2);
+      if(len < 0.35)
+        m = 1;
+      else if(len < 0.6)
+        m = Math.min(m, 2);
+      else if(len < 0.8)
+        m = Math.min(m, 3);
+
+      quads.push(m + 1);
+
+      for(j = 0; j <= m; j++) {
+        px1 = lerp(b1.x, b2.x, j / m);
+        py1 = lerp(b1.y, b2.y, j / m);
+        px2 = lerp(strip[i].x, strip[i1].x, j / m);
+        py2 = lerp(strip[i].y, strip[i1].y, j / m);
+        points.push(
+          { x: lerp(b1.x, b2.x, j / m), y: lerp(b1.y, b2.y, j / m) },
+          { x: lerp(strip[i].x, strip[i1].x, j / m), y: lerp(strip[i].y, strip[i1].y, j / m) }
+        );
+      }
+    }
+    points.push(points[0]);
+
+    for(i = 0, n = block.length; i < n; i++) {
+      p = [];
+      for(j = 0; j < quads[i]; j++) {
+        p.push(points.shift());
+        p.push(points.shift());
+      }
+      p.push(block[(i + 1) % n]);
+      p.push(points[0] || block[0]);
+      ctx.strokeStyle = 'white';
+      for(var k = 0, m = p.length; k < m - 2; k+= 2) {
+        ctx.beginPath();
+        ctx.fillStyle = 'rgb(' + [ Math.random() * 255, Math.random() * 255, Math.random() * 255 ].map(Math.floor).join(',') + ')';
+        rect(p[k], p[(k + 1) % m], p[(k + 3) % m], p[(k + 2) % m]);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+      }
+    }
+  }
+
+  for(var i = 0; i < polys.length; i++) {
+    ctx.fillStyle = 'rgba(0,0,0,0)';
+    polys[i] = {
+      road: polys[i],
+      block: insetPolygon(polys[i], 0.1)
+    };
+    ctx.fillStyle = 'rgba(0,0,0,0)';
+    polys[i].strip = insetPolygon(polys[i].block, 0.2);
+    subdivideStrip(polys[i].block, polys[i].strip);
+  }
+
+  console.log(polys);
+
 
 }());
