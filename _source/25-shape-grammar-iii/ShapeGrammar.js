65d861fe0fe3f51f81b247d4b98447ba1fc2e770
Andrea Venuta <venutawebdesign@gmail.com>
2015-05-05 15:23:37 +0200

    .

---
+++ _source/25-shape-grammar-iii/ShapeGrammar.js
@@ -0,0 +1,392 @@
+var ShapeGrammar = (function() {
+
+  var _ = function() {
+    this.rules = [];
+  };
+
+  _.prototype.define = function(lhs, cond, rhs) {
+    this.rules.push({
+      lhs: lhs,
+      cond: cond,
+      rhs: rhs
+    });
+  };
+
+  _.prototype.run = function(state) {
+    
+    var output = [], rules = this.rules, nonterminals = 0;
+
+    state = (state instanceof Array? state : [state]);
+
+    while(state.length) {
+
+      var lhs = state.shift();
+
+      if(lhs.sym === _.TERMINAL) {
+        output.push(lhs);
+      } else for(var i = 0, I = rules.length; i < I; i++) {
+        
+        var rule = rules[i];
+        if(lhs.sym === rule.lhs && 
+          (rule.cond === null || rule.cond.call(lhs))) {
+          
+          var ret = rule.rhs.call(lhs);
+          ret = (ret instanceof Array? ret : [ret]);
+
+          for(var j = 0, J = ret.length; j < J; j++) {
+            output.push(ret[j]);
+            ++nonterminals;
+          }
+
+          break;
+        }
+      }
+    }
+
+    return (nonterminals > 0 ? this.run(output) : output);
+  }
+
+  _.TERMINAL = 'TERMINAL';
+
+  return _;
+  
+}());
+
+var shg = new ShapeGrammar();
+
+shg.define('Building', null, function() {
+
+  var ret = [],
+      curHeight = 0;
+  
+  for(var i = 0, I = this.floorsLayout.length; i < I; i++) {
+    var fli = this.floorsLayout[i], floor = {
+      sym: fli.type,
+      height: fli.height,
+      params: fli,
+      points: this.points.map(function(i) {
+        return { x: i.x, y: i.y + curHeight, z: i.z };
+      })
+    };
+
+    curHeight += fli.height;
+    
+    ret.push(floor);
+  }
+
+  return ret;
+
+});
+
+shg.define('FL_GndFloor', null, function() {
+  
+  var facades = SHAPE.extrudeAll(this.points, this.height, 'Facade', [0, 1, 0]);
+
+  switch(this.params.tiles) {
+    case 'OneDoor':
+      for(var i = 1, I = facades.length; i < I; i++)
+        facades[i].type = 'Windows';
+      facades[0].type = 'OneDoor';
+      break;
+  }
+
+  return facades;
+
+});
+
+shg.define('FL_Floor', null, function() {
+  
+  var facades = SHAPE.extrudeAll(this.points, this.height, 'Facade', [0, 1, 0]);
+
+  for(var i = 0, I = facades.length; i < I; i++) {
+    facades[i].type = 'Windows';
+    facades[i].windows = this.params.windows;
+  }
+
+  return facades;
+
+});
+
+shg.define('FL_Ledge', null, function() {
+
+  var extrPoints = [], h = this.height;
+
+  for(var i = 0, I = this.points.length; i < I; i++) {
+    var p0 = this.points[i], p1 = this.points[(i + 1) % I],
+        angle = Math.atan2(p1.z - p0.z, p1.x - p0.x),
+        anglep = angle - Math.PI / 2,
+        cos = Math.cos(angle) + Math.cos(anglep),
+        sin = Math.sin(angle) + Math.sin(anglep);
+
+    extrPoints.push({
+      x: p0.x - this.params.width * cos,
+      y: p0.y, 
+      z: p0.z - this.params.width * sin
+    })
+  }
+
+  var facades = SHAPE.extrudeAll(extrPoints, this.height, 'Quad', [0, 1, 0]);
+
+  facades.forEach(function(i) {
+    var dx = i.points[3].x - i.points[0].x,
+        dy = i.points[1].y - i.points[0].y,
+        dz = i.points[3].z - i.points[0].z;
+    var t = h / shg.UVSCALE,
+        s = t * Math.sqrt(dx * dx + dz * dz) / dy;
+
+    i.uvs = [
+      { s: 0, t: t },
+      { s: 0, t: 0 },
+      { s: s, t: 0 },
+      { s: s, t: t }
+    ];
+  });
+
+  facades.push({
+    sym: 'Poly',
+    points: extrPoints
+  });
+
+  facades.push({
+    sym: 'Poly',
+    points: extrPoints.map(function(i) {
+      i.y += h;
+      return i;
+    })
+  });
+
+  return facades;
+
+});
+
+shg.define('FL_Rooftop', null, function() {
+
+  var extrPoints = [], h = this.height;
+
+  for(var i = 0, I = this.points.length; i < I; i++) {
+    var p0 = this.points[i], p1 = this.points[(i + 1) % I],
+        angle = Math.atan2(p1.z - p0.z, p1.x - p0.x),
+        anglep = angle - Math.PI / 2,
+        cos = Math.cos(angle) + Math.cos(anglep),
+        sin = Math.sin(angle) + Math.sin(anglep);
+
+    extrPoints.push({
+      x: p0.x + this.params.width * cos,
+      y: p0.y, 
+      z: p0.z + this.params.width * sin
+    })
+  }
+
+  var facadesOut = SHAPE.extrudeAll(this.points, this.height, 'Quad', [0, 1, 0]),
+      facadesIn  = SHAPE.extrudeAll(extrPoints,  this.height, 'Quad', [0, 1, 0]);
+
+  facadesIn.push({
+    sym: 'Poly',
+    points: extrPoints
+  });
+
+  while(facadesOut.length)
+    facadesIn.push(facadesOut.shift());
+
+  facadesIn.forEach(function(i) {
+    var dx = i.points[3].x - i.points[0].x,
+        dy = i.points[1].y - i.points[0].y,
+        dz = i.points[3].z - i.points[0].z;
+    var t = h / shg.UVSCALE,
+        s = t * Math.sqrt(dx * dx + dz * dz) / dy;
+
+    i.uvs = [
+      { s: 0, t: t },
+      { s: 0, t: 0 },
+      { s: s, t: 0 },
+      { s: s, t: t }
+    ];
+  });
+
+  for(var i = 0, I = extrPoints.length; i < I; i++) {
+    var ii = (i + 1) % I,
+        p0 = this.points[i], p1 = extrPoints[i],
+        p2 = extrPoints[ii], p3 = this.points[ii];
+
+    var poly = {
+      sym: 'Poly',
+      points: [ p0, p1, p2, p0, p2, p3 ].map(function(i) { return { x: i.x, y: i.y + h, z: i.z }; })
+    };
+
+    facadesIn.push(poly);
+  }
+
+  return facadesIn;
+});
+
+shg.define('Facade', function() { return this.type === 'OneDoor' }, function() {
+
+  var quads = SHAPE.fit('x', this, 'Window', 1);
+
+  quads[ ~~(quads.length / 2) ].sym = 'Door';
+  
+  for(var i = 0, I = quads.length; i < I; i++)
+    quads[i].normal = this.normal;
+
+  return quads;
+
+});
+
+shg.define('Facade', null, function() {
+
+  var dx = this.points[3].x - this.points[0].x,
+      dy = this.points[1].y - this.points[0].y,
+      dz = this.points[3].z - this.points[0].z,
+      t  = dy / shg.UVSCALE,
+      s  = t * Math.sqrt(dx * dx + dz * dz) / dy;
+
+  this.uvs = [
+    { s: 0, t: t },
+    { s: 0, t: 0 },
+    { s: s, t: 0 },
+    { s: s, t: t }
+  ];
+
+  var quads = SHAPE.fit('x', this, 'Window', 1);
+
+  //quads[ Math.round(quads.length / 2) ].sym = 'Door';
+  
+  for(var i = 0, I = quads.length; i < I; i++)
+    quads[i].normal = this.normal;
+
+  return quads;
+
+});
+
+shg.define('Window', null, function() {
+  
+  var hsp = SHAPE.split(this, [ .3, .4, .3 ], [1], 'Quad'),
+      vsp = SHAPE.split(hsp[1], [1], [.15, .7, .15], 'Quad');
+
+  vsp[1].uvs = null;
+
+  var ret = [ hsp[0], hsp[2], vsp[0], vsp[2] ];
+
+  ret.push(vsp[1]);
+
+  return ret;
+
+});
+
+shg.define('Quad', null, (function() {
+
+  var defaultUVS = [ 
+    { s: 0, t: 1 }, 
+    { s: 0, t: 0 }, 
+    { s: 1, t: 0 }, 
+    { s: 1, t: 1 } 
+  ];
+
+  return function() {
+    
+    var vertices, normals = [], uvs,
+        normal, texID,
+        u0, u1, u2, u3,
+        p0, p1, p2, p3, ps = this.points;
+
+    p0 = ps[0], p1 = ps[1], p2 = ps[2], p3 = ps[3];
+
+    vertices = [
+      p0.x, p0.y, p0.z, p1.x, p1.y, p1.z, p2.x, p2.y, p2.z,
+      p0.x, p0.y, p0.z, p2.x, p2.y, p2.z, p3.x, p3.y, p3.z
+    ];
+
+    normal = this.normal || Geom.triToNormal(vertices);
+    for(var i = 0; i < 6; i++)
+      normals.push(normal[0], normal[1], normal[2]);
+
+    uvs = this.uvs || defaultUVS;
+    u0 = uvs[0], u1 = uvs[1], u2 = uvs[2], u3 = uvs[3];
+    texID = this.texID || 0;
+
+    uvs = [
+      u0.s, u0.t, texID, u1.s, u1.t, texID, u2.s, u2.t, texID,
+      u0.s, u0.t, texID, u2.s, u2.t, texID, u3.s, u3.t, texID
+    ];
+
+    return {
+      sym: ShapeGrammar.TERMINAL,
+      vertices: vertices,
+      normals: normals,
+      uvs: uvs
+    }
+
+  }
+
+}()));
+
+shg.define('Poly', null, function() {
+  var rings = this.points.map(function(i) {
+    return [i.x, i.z];
+  }), y = this.points[0].y;
+
+  var triverts = earcut([rings]),
+      vertices = triverts.reduce(function(o, i) {
+        o.push(i[0], y, i[1]);
+        return o;
+      }, []),
+      normal = this.normal || Geom.triToNormal(vertices),
+      normals = [], uvs = [];
+
+  var minX, minZ, maxX, maxZ, dx, dz, p;
+
+  minX = minZ = Number.POSITIVE_INFINITY;
+  maxX = maxZ = Number.NEGATIVE_INFINITY;
+
+  for(var i = 0, I = this.points.length; i < I; i++) {
+    p = this.points[i];
+    if(minX > p.x) minX = p.x;
+    if(maxX < p.x) maxX = p.x;
+    if(minZ > p.z) minZ = p.z;
+    if(maxZ < p.z) maxZ = p.z;
+
+    normals.push(normal[0], normal[1], normal[2]);
+  }
+
+  for(var i = 0, I = vertices.length; i < I; i += 3) {
+    var x = vertices[i], z = vertices[i + 2];
+    uvs.push( (x - minX) / shg.UVSCALE, (z - minZ) / shg.UVSCALE, 0 );
+  }
+
+  return {
+    sym: ShapeGrammar.TERMINAL,
+    vertices: vertices,
+    normals: normals,
+    uvs: uvs
+  }
+
+})
+
+var pts = [];
+
+for(var i = 0; i < 16; i++)
+  pts.push({ x : .3 * Math.cos(-i * Math.PI / 8), y: 0, z: .3 * Math.sin(-i * Math.PI / 8) });
+
+shg.UVSCALE = .2;
+
+var shgResult = shg.run({
+  sym: 'Building',
+  floorsLayout: [
+    { type: 'FL_GndFloor', height: .2, tiles: 'OneDoor' },
+    { type: 'FL_Ledge',    height: .025, width: .0125 },
+    { type: 'FL_Floor',    height: .1, windows: 'Double' },
+    { type: 'FL_Floor',    height: .1, windows: 'Double' },
+    { type: 'FL_Floor',    height: .1, windows: 'Double' },
+    { type: 'FL_Floor',    height: .1, windows: 'Double' },
+    { type: 'FL_Ledge',    height: .025, width: .0125 },
+    { type: 'FL_Floor',    height: .15, windows: 'Single' },
+    { type: 'FL_Rooftop',  height: .05, width: .025 }
+  ],
+  points: [
+    { x: -.3, y: 0, z: -.3 },
+    { x: -.3, y: 0, z:  .3 },
+    { x:  .3, y: 0, z:  .3 },
+    { x:  .3, y: 0, z: -.3 }
+  ]
+  //points: pts
+});
---
+++ _source/25-shape-grammar-iii/script.js
@@ -0,0 +1,236 @@
+var SIDE = 200;
+
+var Mesh = function(gl, geom) {
+  this.vbuf = gl.createBuffer();
+  this.nbuf = gl.createBuffer();
+  this.cbuf = gl.createBuffer();
+
+  this.count = ~~(geom.vertices.length / 3);
+
+  this.gl = gl;
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, this.vbuf);
+  gl.bufferData(gl.ARRAY_BUFFER, geom.vertices, gl.STATIC_DRAW);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, this.nbuf);
+  gl.bufferData(gl.ARRAY_BUFFER, geom.normals, gl.STATIC_DRAW);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, this.cbuf);
+  gl.bufferData(gl.ARRAY_BUFFER, geom.uvs, gl.STATIC_DRAW);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+}
+
+Mesh.prototype.bind = function(program) {
+  var gl = this.gl;
+  gl.bindBuffer(gl.ARRAY_BUFFER, this.vbuf);
+  gl.enableVertexAttribArray(gl.getAttribLocation(program, 'vertex'));
+  gl.vertexAttribPointer(gl.getAttribLocation(program, 'vertex'), 3, gl.FLOAT, false, 0, 0);
+  gl.bindBuffer(gl.ARRAY_BUFFER, this.nbuf);
+  gl.enableVertexAttribArray(gl.getAttribLocation(program, 'normal'));
+  gl.vertexAttribPointer(gl.getAttribLocation(program, 'normal'), 3, gl.FLOAT, false, 0, 0);
+  gl.bindBuffer(gl.ARRAY_BUFFER, this.cbuf);
+  gl.enableVertexAttribArray(gl.getAttribLocation(program, 'uv'));
+  gl.vertexAttribPointer(gl.getAttribLocation(program, 'uv'), 3, gl.FLOAT, false, 0, 0);
+
+}
+
+Mesh.prototype.unbind = function() {
+  this.gl.bindBuffer(gl.ARRAY_BUFFER, null);
+}
+
+Object.defineProperty(Mesh.prototype, 'model', {
+  set: function(matrix) {
+    this.modelMatrix = matrix;
+  }
+});
+
+var Shader = function(gl, vshSrc, fshSrc) {
+  var program = gl.createProgram(),
+      vsh = gl.createShader(gl.VERTEX_SHADER),
+      fsh = gl.createShader(gl.FRAGMENT_SHADER);
+
+  gl.getExtension('OES_standard_derivatives');
+
+  gl.shaderSource(vsh, vshSrc);
+  gl.shaderSource(fsh, fshSrc);
+  gl.compileShader(vsh);
+  gl.compileShader(fsh);
+  gl.attachShader(program, vsh);
+  gl.attachShader(program, fsh);
+  gl.linkProgram(program);
+
+  return program;
+}
+
+var Scene = function(gl) {
+  this.gl = gl;
+  this.graph = [];
+  this.matrices = {
+    projection: mat4.create(),
+    view: mat4.create(),
+    shadowProjection: mat4.create(),
+    shadowView: mat4.create(),
+    lightPos: vec3.create()
+  };
+
+  this.shFb = gl.createFramebuffer();
+  this.shTex = gl.createTexture();
+  this.shRb = gl.createRenderbuffer();
+
+  this.program = Shader(gl, document.getElementById('vertex-shader').textContent, 
+                        document.getElementById('fragment-shader').textContent);
+
+  gl.useProgram(this.program);
+}
+
+Scene.prototype.add = function(el) {
+  this.graph.push(el);
+}
+
+Scene.prototype.draw = function(w, h) {
+  var ltransf = mat4.create(), 
+      shView = mat4.create(),
+      gl = this.gl;
+
+  gl.viewport(0, 0, w, h);
+  gl.clearColor(0, 0, 0, 1);
+  gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+
+  gl.uniformMatrix4fv(gl.getUniformLocation(this.program, 'projection'), false, this.matrices.projection);
+  gl.uniformMatrix4fv(gl.getUniformLocation(this.program, 'view'), false, this.matrices.view);
+  gl.uniform3fv(gl.getUniformLocation(this.program, 'lightPos'), this.matrices.lightPos);
+
+  this.graph.forEach((function(program) { return function(i) {
+    i.bind(program);
+    var nmatrix = mat3.create();
+    mat3.normalFromMat4(nmatrix, i.modelMatrix);
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, 'model'), false, i.modelMatrix);
+    gl.uniformMatrix3fv(gl.getUniformLocation(program, 'nmatrix'), false, nmatrix);
+    gl.drawArrays(gl.TRIANGLES, 0, i.count);
+
+  }}(this.program)));
+}
+
+Scene.prototype.setView = function(view) {
+  mat4.copy(this.matrices.view, view);
+}
+
+Object.defineProperty(Scene.prototype, "projection", {
+  set: function(p) {
+    mat4.copy(this.matrices.projection, p);
+  }
+});
+Object.defineProperty(Scene.prototype, "view", {
+  set: function(p) {
+    mat4.copy(this.matrices.view, p);
+  }
+});
+Object.defineProperty(Scene.prototype, "lightPos", {
+  set: function(p) {
+    vec3.copy(this.matrices.lightPos, p);
+  }
+});
+
+//console.log(shg);
+
+(function() {
+
+  var canvas = document.getElementById('canvas'),
+      gl     = canvas.getContext('webgl'),
+      bcr    = canvas.getBoundingClientRect(),
+      w      = bcr.width,
+      h      = bcr.height,
+      stats  = new Stats();
+
+  stats.setMode(0);
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.right = '1rem';
+  stats.domElement.style.top = '1rem';
+  document.body.appendChild(stats.domElement);
+
+  canvas.width = w;
+  canvas.height = h;
+
+  gl.viewport(0, 0, w, h);
+
+  gl.enable(gl.DEPTH_TEST);
+  gl.depthFunc(gl.LESS);
+  
+  var proj = mat4.create(),
+      view = mat4.create(),
+      model = mat4.create(),
+      lightPos = vec3.fromValues(1, 0.25, -1),
+      shProj = mat4.create(),
+      scene = new Scene(gl),
+      ext = gl.getExtension('EXT_texture_filter_anisotropic');
+
+  (function() {
+    var vertices = [], //shgResult.vertices,
+        normals = [], //shgResult.normals,
+        uvs = []; //shgResult.colors;
+
+
+    for(var i = 0, I = shgResult.length; i < I; i++) {
+      var sr = shgResult[i];
+      for(var j = 0, J = sr.vertices.length; j < J; j++) {
+        vertices.push(sr.vertices[j]);
+        normals.push(sr.normals[j]);
+        uvs.push(sr.uvs[j]);
+      }
+    }
+
+    var bldg = new Mesh(gl, { vertices: new Float32Array(vertices), normals: new Float32Array(normals), uvs: new Float32Array(uvs) });
+    bldg.model = model;
+    scene.add(bldg);
+  }());
+
+  mat4.perspective(proj, Math.PI / 2, w / h, 0.0001, 1000.0);
+  mat4.ortho(shProj, -16, 16, -16, 16, -16, 16);
+
+  mat4.identity(view);
+  mat4.translate(view, view, [0, -.5, -1]);
+
+  scene.projection = proj;
+  scene.shadowProjection = shProj;
+  scene.view = view;
+  var rX = 0, rY = 0;
+
+  canvas.addEventListener('mousedown', function(evt) {
+    
+    var x = evt.clientX, y = evt.clientY, irx = rX, iry = rY;
+    var omm = function(evt) {
+      evt.preventDefault();
+      evt.stopPropagation();
+      rY = iry + (x - evt.clientX) / 128;
+      rX = irx + (y - evt.clientY) / 128;
+    }, omu = function(evt) {
+      canvas.removeEventListener('mousemove', omm);
+      canvas.removeEventListener('mouseup', omu);
+    }
+
+    canvas.addEventListener('mousemove', omm);
+    canvas.addEventListener('mouseup', omu);
+
+  });
+
+  function render() {
+
+    stats.begin();
+    scene.view = view;
+    mat4.identity(model);
+    mat4.rotateY(model, model, rY);
+    mat4.rotateX(model, model, rX);
+
+    render.t += 0.02;
+
+    scene.lightPos = lightPos;
+    scene.draw(w, h);
+    stats.end();
+
+    requestAnimationFrame(render);
+  }
+  render.t = 0;
+  render();
+
+}());
